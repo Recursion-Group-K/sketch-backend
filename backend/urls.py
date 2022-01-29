027c4adf21b821e439fb988db99c38da49b10f1c
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-28 23:05:09 -0800

    add url endpoints

--- backend/urls.py
+++ backend/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('user/', include('user.urls')),
-    path('drawing/', include('drawing.urls')),
+    # path('user/', include('user.urls')),
+    # path('drawing/', include('drawing.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token-auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token-auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/users/', include('user.urls')),
+    path('api/drawings/', include('drawing.urls')),
 ]
