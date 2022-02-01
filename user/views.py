b729fca973b1cc6aa339892a9825da51d526bd50
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-31 22:43:59 -0800

    add viewset and action decorator

--- user/views.py
+++ user/views.py
@@ -1,19 +1,20 @@
 from .models import User
 from .serializer import UserSerializer
-from rest_framework import generics, permissions
-# from rest_framework.decorators import action
-# from rest_framework.response import Response
+from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from user.permission import UserObjPersmission
 
 # Create your views here.
-class UserList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
   permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
   filter_fields = ('is_superuser', 'is_active')
 
-  # def highlight(self, request, *args, **kwargs):
-  #   return Response(request.user)
+  @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+  def highlight(self, request, *args, **kwargs):
+    return Response(request.user)
 
   def perform_create(self, serializer):
     data = serializer.data
