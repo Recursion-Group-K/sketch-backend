d225a2ce0881c2afe7bde8213cbb72640467f5f1
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-31 22:58:39 -0800

    remove user_highlight from user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,4 @@
 from user.views import UserViewSet
-from rest_framework import renderers
 from user import views
 from django.urls import path
 
@@ -8,12 +7,8 @@ user_list = UserViewSet.as_view({
   'post': 'create'
 })
 
-user_highlight = UserViewSet.as_view({
-  'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
 
 urlpatterns = [
   path('', user_list, name='user_list'),
   path('<int:pk>/', views.UserRetrieveUpdateDestroy.as_view()),
-  path('', user_highlight, name='user_highlight'),
 ]
