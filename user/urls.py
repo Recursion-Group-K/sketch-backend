76d439145aeafa07ee18726f6a2b2a80fc44ac7c
Taesok Kwon <kwontaesok1997@gmail.com>
2022-01-31 22:43:28 -0800

    binding viewsets to urls explicitly

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from user.views import UserViewSet
+from rest_framework import renderers
 from user import views
+from django.urls import path
+
+user_list = UserViewSet.as_view({
+  'get': 'list',
+  'post': 'create'
+})
+
+user_highlight = UserViewSet.as_view({
+  'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
 
 urlpatterns = [
-  path('', views.UserList.as_view()),
-  path('<int:pk>/', views.UserRetrieveUpdateDestroy.as_view())
+  path('', user_list, name='user_list'),
+  path('<int:pk>/', views.UserRetrieveUpdateDestroy.as_view()),
+  path('', user_highlight, name='user_highlight'),
 ]
