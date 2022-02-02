2bbb614735e376fa1521b9c71a72935d39c30016
Taesok Kwon <kwontaesok1997@gmail.com>
2022-02-02 04:22:08 -0800

    create endpoint test module

---
+++ user/tests/test_models.py
@@ -0,0 +1,40 @@
+from multiprocessing.sharedctypes import Value
+from rest_framework.test import APITestCase
+from ..models import User
+
+class TestModel(APITestCase):
+
+  def test_create_user(self):
+    user = User.objects.create_user('test', 'test@gmail.com', 'test123')
+
+    self.assertIsInstance(user, User)
+    self.assertFalse(user.is_staff)
+    self.assertFalse(user.is_superuser)
+    self.assertEqual(user.username, "test")
+    self.assertEqual(user.email, "test@gmail.com")
+
+  def test_create_super_user(self):
+    user = User.objects.create_superuser('test', 'test@gmail.com', 'test123')
+
+    self.assertIsInstance(user, User)
+    self.assertTrue(user.is_staff)
+    self.assertTrue(user.is_superuser)
+    self.assertEqual(user.username, "test")
+    self.assertEqual(user.email, "test@gmail.com")
+    
+  def test_raise_error_in_create_user(self):
+    # creating user with no username should raise error 
+    self.assertRaises(ValueError, User.objects.create_user, username='', email='test@gmail.com', password='test123')
+    # make sure the error message is the one we set 
+    with self.assertRaisesMessage(ValueError, 'The given username must be set'):
+      User.objects.create_user(username='', email='test@gmail.com', password='test123')
+    self.assertRaises(ValueError, User.objects.create_user, username='test', email='', password='test123')
+    with self.assertRaisesMessage(ValueError, 'The given email must be set'):
+      User.objects.create_user(username='test', email='', password='test123')
+
+  def test_raise_error_in_super_user(self):
+    with self.assertRaisesMessage(ValueError, 'Superuser must have is_staff=True.'):
+      User.objects.create_superuser(username='test', email='test@gmail.com', password='test123', is_staff=False)
+
+    with self.assertRaisesMessage(ValueError, 'Superuser must have is_superuser=True.'):
+      User.objects.create_superuser(username='test', email='', password='test123', is_superuser=False)
---
+++ user/tests/test_urls.py
@@ -0,0 +1,75 @@
+from urllib import response
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from ..models import User
+
+# Create your tests here.
+class TestTokenAuth(APITestCase):
+
+  def get_token(self):
+    url = reverse('token_obtain_pair')
+    user = User.objects.create_user(username="test", email="test@gmail.com", password="test123")
+    user.is_active = True
+    user.save()
+    response = self.client.post(url, {"username": "test", "password": "test123"}, format='json')
+    token = response.data['access']
+    return token
+
+  def test_api_token_auth(self):
+    # 構築
+    url = reverse('token_obtain_pair')
+    user = User.objects.create_user(username="test", email="test@gmail.com", password="test123")
+    # 操作
+    user.is_active = False
+    user.save()
+
+    response = self.client.post(url, {"email": "test@gmail.com", "password": "test123"}, format='json')
+    # 検証
+    self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    # 操作
+    user.is_active = True
+    user.save()
+
+    response = self.client.post(url, {"username": "test", "password": "test123"}, format='json')
+    # 検証
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertTrue('access' in response.data)
+
+class TestUserEndPoints(APITestCase):
+
+  def get_token(self):
+    url = reverse('token_obtain_pair')
+    user = User.objects.create_user(username="test", email="test@gmail.com", password="test123")
+    user.is_active = True
+    user.save()
+    response = self.client.post(url, {"username": "test", "password": "test123"}, format='json')
+    token = response.data['access']
+    print("Token: " + token)
+    return token
+
+  # def test_api_current_user(self):
+
+  #   url = '/api/current_user/'
+  #   # self.client.credentials(HTTP_AUTHORIZATION='Token ' + 'faketoken')
+  #   # response = self.client.get(url, data={'format': 'json'})
+  #   # self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+  #   token = self.get_token()
+  #   self.client.credentials(HTTP_AUTHORIZATION='Token' + token)
+  #   response = self.client.get(url, data={'format': 'json'})
+  #   self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_api_user_list(self):
+
+    url = '/api/users/'
+    self.client.credentials(HTTP_AUTHORIZATION='Token' + 'faketoken')
+    response = self.client.get(url, data={'format': 'json'})
+    self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    token = self.get_token()
+    self.client.credentials(HTTP_AUTHORIZATION='Token' + token)
+    response = self.client.get(url, data={'format': 'json'})
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
