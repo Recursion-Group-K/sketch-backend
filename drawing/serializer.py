e722db3b69d8d97e1de689e4c8dca0c545ff8b44
Leandro <72016706+mcnLeandro@users.noreply.github.com>
2022-02-04 17:12:18 +0900

    fix: #30

--- drawing/serializer.py
+++ drawing/serializer.py
@@ -1,3 +1,4 @@
+from urllib import request
 from rest_framework import serializers
 from .models import Drawing
 from user.models import User
@@ -5,18 +6,10 @@ from user.serializer import UserSerializer
 
 class DrawingSerializer(serializers.ModelSerializer):
   user = UserSerializer(read_only=True)
-  user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True) #write_only=Trueを指定する事によってGET時にこのフィールドを出さないようにする
+  user_id = serializers.PrimaryKeyRelatedField(
+    read_only=True, 
+    default=serializers.CurrentUserDefault()
+  )
   class Meta:
     model = Drawing
     fields = ['id', 'title', 'image', 'data', 'is_public', 'created_at', 'updated_at', 'user', 'user_id', 'mode'] 
-
-  def create(self, validated_data):
-    validated_data['user'] = validated_data.get('user_id', None)
-
-    if validated_data['user'] is None:
-      raise serializers.ValidationError('user not found.')
-    
-    # delete object
-    del validated_data['user_id']
-    
-    return Drawing.objects.create(**validated_data)
